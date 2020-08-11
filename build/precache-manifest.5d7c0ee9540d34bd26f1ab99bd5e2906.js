d04aede6c3647aec67752f3582948fbd25d35e0a
DaclynsDesign <68850127+DaclynsDesign@users.noreply.github.com>
2020-08-11 19:34:57 +0800

    Add files via upload
    GTL-Week3 Assignment

---
+++ build/precache-manifest.5d7c0ee9540d34bd26f1ab99bd5e2906.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c5fd8b02ab531380d950bc8dc84da4b0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "03868ea4561cd0b4cbf8",
+    "url": "/static/css/main.bfc225ea.chunk.css"
+  },
+  {
+    "revision": "68c16593845476c5f213",
+    "url": "/static/js/2.4ad83167.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.4ad83167.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "03868ea4561cd0b4cbf8",
+    "url": "/static/js/main.471f4f97.chunk.js"
+  },
+  {
+    "revision": "6b771f4da7321e882f74",
+    "url": "/static/js/runtime-main.373a8406.js"
+  },
+  {
+    "revision": "5f1cd582c6d4fb5f4d86e6c1635deec2",
+    "url": "/static/media/hulk-1.5f1cd582.jpg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "294dd39b7ab19aedb004eb8f359c307f",
+    "url": "/static/media/venom-1.294dd39b.jpg"
+  },
+  {
+    "revision": "5eb62fe8d11e8a32b2fb2554088f308e",
+    "url": "/static/media/venom-2.5eb62fe8.jpg"
+  }
+]);
